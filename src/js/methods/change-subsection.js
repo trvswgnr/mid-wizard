04170e3bd0af05219decbe423881563468afa1b4
trvswgnr <trvswgnr@gmail.com>
2017-09-27 14:14:15 -0400

    ¯\_(ツ)_/¯

---
+++ src/js/methods/change-subsection.js
@@ -0,0 +1,41 @@
+// activate subsection by adding class to next and removing from current
+subsection.activate = function () {
+  subsection.c.removeClass("active").addClass('subsection-done');
+  subsection.n.addClass("active").removeClass('subsection-done');
+}
+
+/**
+ * Advance to the next subsection.
+ */
+Wizard.change_subsection = function () {
+  // go to the next subsection
+  $("button.get-next-subsection").click(function (e) {
+    e.preventDefault();
+
+    //show buttons after animation
+    setTimeout(function () {
+      btns.show();
+    }, animation_time);
+
+    //subsection
+    subsection.c = $(Wizard.settings.subsection_el + '.active');
+    subsection.n = $data('subsection-order', (subsection.count + 1));
+    subsection.activate();
+    subsection.count = subsection.n.data("subsection-order");
+
+    // view
+    view.c = subsection.c.find(Wizard.settings.view_el + ':last');
+    view.count = view.c.data("view");
+    view.n = $('[data-view="' + (view.count + 1) + '"]');
+    view.activate();
+    view.count = view.n.data("view");
+
+    // nav
+    $data('nav-subsection', subsection.count).addClass("active");
+    $data('nav-subsection', (subsection.count - 1)).addClass("done");
+
+    // change label texts back to normal
+    change_label_text(false);
+  });
+
+}
