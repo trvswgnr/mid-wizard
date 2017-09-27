a634ac33d0712ad1192c568e028ca85cf6965fff
trvswgnr <trvswgnr@gmail.com>
2017-09-27 09:20:39 -0400

    added function to go to specific section in wizard

--- src/js/functions/change-section.js
+++ src/js/functions/change-section.js
@@ -1,5 +1,25 @@
-Wizard.change_section = function () {
+Wizard.go_section = function (section_number) {
+  $('section').removeClass('active');
+  section.count = section_number;
+  section.c = $('section.active');
+  section.n = $data('section',section.count);
+  section.n.addClass('active');
 
+  $('.subsection').removeClass('active');
+  subsection.count = section.n.find('.subsection:first').data('subsection-order');
+  subsection.n =  section.n.find('.subsection:first');
+  subsection.n.addClass('active').removeClass('subsection-done');
+
+  $('fieldset').removeClass('active');
+  view.count = subsection.n.find('fieldset:first').data('view');
+  view.n = $data('view', view.count);
+  view.n.addClass('active').removeClass('done');
+}
+
+/**
+ * CHANGE SECTION
+ */
+Wizard.change_section = function () {
   // go to the next subsection
   $("button.get-next-section").click(function () {
     // section
@@ -25,6 +45,7 @@ Wizard.change_section = function () {
     view.n = subsection.n.find('fieldset:first');
     view.activate();
     view.count = view.n.data("view");
+    change_label_text(false);
 
   });
 }
