3cd37e90978e4d5f1593710d2a3a122bdda45801
trvswgnr <trvswgnr@gmail.com>
2017-09-28 15:15:23 -0400

    rename

--- src/js/modules/events.js
+++ src/js/modules/events.js
@@ -2,19 +2,15 @@
  * Initialize Click / Action Events
  */
 Wizard.events = function () {
-
   // prevent default button functionality
   btns.click(function (e) {
     e.preventDefault();
   });
-
   Wizard.change_view();
-
   Wizard.review_subsection();
-
   Wizard.change_subsection();
-
   Wizard.change_section();
 //  Wizard.nav_subsection();
-  Wizard.devnav();
+  dev_tools(); // remove at production
+  conditionals();
 }
