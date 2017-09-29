52ebffa322d840db34080480c0c666b8ea5a826b
trvswgnr <trvswgnr@gmail.com>
2017-09-29 10:38:01 -0400

    what is the scope?

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,28 @@
 /**
- * Initialize the Wizard.
+ * Initialize the app.
+ * @namespace
+ * @arg {Object} options - Options that override the defaults.
+ * @arg {Object} options.elements - Element tag or class names.
+ * @arg {string} [options.elements.section=section] - Tag or class name for targeting 'sections' in the DOM.
+ * @arg {string} [options.elements.subsection=.subsection] - Tag or class name for targeting 'subsections' in the DOM.
+ * @arg {string} [options.elements.view=fieldset] - Tag or class name for targeting 'views' in the DOM.
  */
-Wizard.init = function (options = {}) {
-  $.extend(true, Wizard.settings, options);
+let init = function (options = {}) {
+  let settings = {
+    elements: {
+      section: "section",
+      subsection: ".subsection",
+      view: "fieldset"
+    }
+  }
+  $.extend(true, settings, options);
+  section.el = settings.elements.section;
+  subsection.el = settings.elements.subsection;
+  view.el = settings.elements.view;
 
   Wizard.startup();
 
   Wizard.events();
-}
+};
 
-Wizard.init();
+init();
