9c6b8daf8322404aa6b4186fa3e79fce0ee54210
trvswgnr <trvswgnr@gmail.com>
2017-09-29 10:37:41 -0400

    i forgot what i was doing

--- src/js/abstracts/variables.js
+++ src/js/abstracts/variables.js
@@ -6,24 +6,35 @@
  * Primary Wizard Object
  * @namespace
  */
-var Wizard = {
-  settings: {
-    section_el: "section",
-    subsection_el: ".subsection",
-    view_el: "fieldset"
-  }
+var Wizard = function() {
+
 };
 
+/**
+ * Object for storing form input data.
+ */
 let input = {},
+  /**
+   * Section element, count, next, current, and previous.
+   */
   section = {
     count: 1
   },
+  /**
+   * Subsection element, count, next, current, and previous.
+   */
   subsection = {
     count: 1
   },
+  /**
+   * 'View' element, count, next, current, and previous.
+   */
   view = {
     count: 1
   },
   btns = $("button");
 
+/**
+ * Match animation time to css animation transition.
+ */
 let animation_time = 400;
