400cfed91f6425c6cc7f4c75a77e15e76bfad1fb
trvswgnr <trvswgnr@gmail.com>
2017-09-29 10:38:31 -0400

    ¯\_(ツ)_/¯

--- src/js/methods/change-section.js
+++ src/js/methods/change-section.js
@@ -1,20 +1,20 @@
 Wizard.go_section = function (section_number) {
   // section
-  $(Wizard.settings.section_el).removeClass('active');
+  $(section.el).removeClass('active');
   section.count = section_number;
-  section.c = $(Wizard.settings.section_el + '.active');
-  section.n = $data(Wizard.settings.section_el, section.count);
+  section.c = $(section.el + '.active');
+  section.n = $data(section.el, section.count);
   section.n.addClass('active');
 
   // subsection
-  $(Wizard.settings.subsection_el).removeClass('active');
-  subsection.count = section.n.find(Wizard.settings.subsection_el + ':first').data('subsection-order');
-  subsection.n = section.n.find(Wizard.settings.subsection_el + ':first');
+  $(subsection.el).removeClass('active');
+  subsection.count = section.n.find(subsection.el + ':first').data('subsection-order');
+  subsection.n = section.n.find(subsection.el + ':first');
   subsection.n.addClass('active').removeClass('subsection-done');
 
   // view
-  $(Wizard.settings.view_el).removeClass('active');
-  view.count = subsection.n.find(Wizard.settings.view_el + ':first').data('view');
+  $(view.el).removeClass('active');
+  view.count = subsection.n.find(view.el + ':first').data('view');
   view.n = $data('view', view.count);
   view.n.addClass('active').removeClass('done');
 }
@@ -26,7 +26,7 @@ Wizard.change_section = function () {
   // go to the next subsection
   $("button.get-next-section").click(function () {
     // section
-    section.c = $(Wizard.settings.section_el + '.active');
+    section.c = $(section.el + '.active');
     section.n = $data('section', (section.count + 1));
     section.c.removeClass('active');
     section.n.addClass('active');
@@ -36,7 +36,7 @@ Wizard.change_section = function () {
     $data('nav-section', (section.count - 1)).addClass("done");
 
     // subsection
-    subsection.c = $(Wizard.settings.subsection_el + '.active');
+    subsection.c = $(subsection.el + '.active');
     subsection.n = $data('subsection-order', (subsection.count + 1));
     subsection.activate();
     subsection.count = subsection.n.data("subsection-order");
@@ -45,7 +45,7 @@ Wizard.change_section = function () {
     $data('nav-subsection', (subsection.count - 1)).addClass("done");
 
     // view
-    view.n = subsection.n.find(Wizard.settings.view_el + ':first');
+    view.n = subsection.n.find(view.el + ':first');
     view.activate();
     view.count = view.n.data("view");
     change_label_text(false);
